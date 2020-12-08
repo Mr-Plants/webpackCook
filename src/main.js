d20691d063c2b469a9b6630c774d16cf2deaaea4
weiyb <weiyb@jiedaibao.com>
2020-12-08 22:12:48 +0800

    hmr cook

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 console.log(Math.random())
 document.write('hello world 333')
+const div = document.createElement('div');
+div.innerText = 'hello webpack'
+document.body.appendChild(div)
 
 // 关键先生，如果不加这里浏览器会刷新
 if (module.hot) {
